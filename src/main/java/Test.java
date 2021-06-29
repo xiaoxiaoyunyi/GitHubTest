a0f0ea7f986b3678b240d1270d9d61e80b3e47b8
xiaoxiaoyunyi <32724739+xiaoxiaoyunyi@users.noreply.github.com>
2021-06-29 16:22:43 +0800

    Update Test.java
    演示拉取pull

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("演示推送push");
+        System.out.println("演示拉取pull");
+        
     }
 }
