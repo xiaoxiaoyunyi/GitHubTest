b6a5016ac7f2bcb24150c5562f4a907bf44cfd37
zhb <2371472031@qq.com>
2021-06-29 16:45:04 +0800

    第二次push

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,7 @@ public class Test {
         System.out.println("Hello World");
         System.out.println("演示推送push");
         System.out.println("演示拉取pull");
+        System.out.println("演示推送push2");
         
     }
 }
